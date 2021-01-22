cf9b73edb312b0e7863b03db0dc6bc3d6dc49577
oluwaseun bello <bellomsean@gmail.com>
2021-01-22 14:57:56 +0100

    set up form connect mongoose

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const articlesRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articlesRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  const articles = [{
+    title: "my title"
+  }]
+  res.render('articles/index', { articles: articles})
 })
 
 app.listen(5000)
